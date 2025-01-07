328db700e98304897c931128ad1353bc0db12af0
Gokul M R <gokulmr331@gmail.com>
2025-01-07 21:43:14 +0530

    pages route setup complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredits from './pages/BuyCredits'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Routes path='/' element={<Home/>} />
+        <Routes path='/result' element={<Result/>} />
+        <Routes path='/buy' element={<BuyCredits/>} />
+      </Routes>
     </div>
   )
 }
