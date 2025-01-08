87b3cab15426dd9fbb181d5650a00df676a93c90
Gokul M R <gokulmr331@gmail.com>
2025-01-08 08:43:12 +0530

    navbar and app chnages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredits from './pages/BuyCredits'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:PX-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Navbar />
       <Routes>
-        <Routes path='/' element={<Home/>} />
-        <Routes path='/result' element={<Result/>} />
-        <Routes path='/buy' element={<BuyCredits/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/result' element={<Result/>} />
+        <Route path='/buy' element={<BuyCredits/>} />
       </Routes>
     </div>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      
+      <Link to='/'>
+      <img src="" alt="imagify" className='w-28 sm:w-32 lg:w-40' />
+      </Link>
     </div>
   )
 }
