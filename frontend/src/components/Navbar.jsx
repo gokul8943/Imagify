f1767f433d8ed59056d87a288adefb9b8318af1f
Gokul M R <gokulmr331@gmail.com>
2025-01-08 09:15:21 +0530

    pages changes are fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Link,useNavigate} from 'react-router-dom'
 const Navbar = () => {
+
+  const [user, setUser] = useState(null)
+  const navigate = useNavigate()
+
+  const handleMove = () =>{
+    navigate('/buy')
+  }
   return (
-    <div>
+    <div className='flex item-center justify-between py-4'>
       <Link to='/'>
-      <img src="" alt="imagify" className='w-28 sm:w-32 lg:w-40' />
+        <img src="" alt="imagify" className='w-28 sm:w-32 lg:w-40' />
       </Link>
+      <div>
+        {user ?
+          <div></div> :
+          <div className='flex item-center gap-2 sm:gap-5'>
+            <p onClick={handleMove} className='cursor-pointer'>Pricing</p>
+            <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+          </div>
+        }
+
+      </div>
     </div>
   )
 }
