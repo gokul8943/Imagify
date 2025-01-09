9866a93d2a70f3142072d0b7b09c8e48e43eee89
Gokul M R <gokulmr331@gmail.com>
2025-01-09 08:49:49 +0530

    working steps added in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Step from '../components/Step'
 
 const Home = () => {
   return (
     <div>
      <Header />
+     <Step />
     </div>
   )
 }
---
+++ frontend/src/utils/WorkData.js
@@ -0,0 +1,14 @@
+export const stepsData = [
+    {
+        title:"Describe Your vision",
+        description:"TYpe a Pharse,sentence, or paragraph that describes the image you eant to create."
+    },
+    {
+        title:"Watch the Magic ",
+        description:"TYpe a Pharse,sentence, or paragraph that describes the image you eant to create."
+    },
+    {
+        title:"Download & Share",
+        description:"TYpe a Pharse,sentence, or paragraph that describes the image you eant to create."
+    }
+]
