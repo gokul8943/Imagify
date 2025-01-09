6b549fb811a478929ce8c0f3c64246c3ac6717d8
Gokul M R <gokulmr331@gmail.com>
2025-01-09 17:14:14 +0530

    route for result page added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import cat from '../assets/cat.png'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+
+ const navigate = useNavigate()
+
+  const handleMove = () =>{
+    navigate('/result')
+  }
+
   return (
     <div className='flex flex-col justify-between items-center text-cnter my-20'>
       <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-nuetral-500'>
@@ -9,7 +17,7 @@ const Header = () => {
       </div>
       <h1 className='text-4xl font-semibold max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center drop-shadow-md'>Turn text to <span className='text-blue-500'>image</span>,in seconds</h1>
       <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity  with AI. Turn your imagination into visual art in seconds just type, and watch the magic happen.</p>
-      <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex item-center gap-2 rounded-full'>Generate Images</button>
+      <button onClick={handleMove} className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex item-center gap-2 rounded-full'>Generate Images</button>
         <div className='flex flex-wrap justify-center mt-16 gap-3'>
           {Array(6).fill('').map((item,index) =>(
             <img className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10'
