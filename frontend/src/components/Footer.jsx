8380ea041a96b2891c2e366c99df46b5b489c95d
Gokul M R <gokulmr331@gmail.com>
2025-01-09 14:57:33 +0530

    footer added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex item-center justify-between gap-4 py-3 mt-20'>
+      <p className='text-orange-600 drop-shadow-lg font-bold'>IMA<span className='text-blue-500'>GIFY </span></p>
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @imagify.com | All right reserved</p>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
