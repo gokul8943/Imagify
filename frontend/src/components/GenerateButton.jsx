25f3e3e3d81575e56e5ee919b8430399c41e81db
Gokul M R <gokulmr331@gmail.com>
2025-01-11 00:10:43 +0530

    animation and conditional checking for generate button

--- frontend/src/components/GenerateButton.jsx
+++ frontend/src/components/GenerateButton.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import { AppContext } from '../context/AppContext'
+
 
 const GenerateButton = () => {
 
+  const {user} = useContext(AppContext)
   const navigate = useNavigate()
 
-  const handleMove = () =>{
-    navigate('/result')
+  const handleMove = () => {
+    if(user){
+      navigate('/result')
+    }else{
+      navigate('/login')
+    }
+    
   }
 
   return (
-    <div className='pb-16 text-center'>
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className='pb-16 text-center'>
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 font-semibolod text-neutral-800 py-6 md:py-16'>See the magic. Try now </h1>
       <button
-      onClick={handleMove}
-      className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500'
+        onClick={handleMove}
+        className='inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500'
       >Generate Image</button>
-    </div>
+    </motion.div>
   )
 }
 
