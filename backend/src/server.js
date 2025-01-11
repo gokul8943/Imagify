3e37c16afaf730d630ce26f71768e90346a294f3
Gokul M R <gokulmr331@gmail.com>
2025-01-11 10:41:18 +0530

    server added

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () =>{
+    console.log("Server is runnning on port" + PORT);
+    
+})
