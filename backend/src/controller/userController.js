2469e904c5a8699ffea7faa418253a22ad659e3d
Gokul M R <gokulmr331@gmail.com>
2025-01-13 14:44:41 +0530

    login and register api completed

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -13,15 +13,10 @@ import jwt from 'jsonwebtoken'
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
 
-        const userData = {
-            name,
-            email,
-            password: hashedPassword
-        }
-        const newUser = new userModel(userData)
-        const user = await newUser.save()
+        const newUser = new userModel({ name, email, password:hashedPassword });
+           const user =   await newUser.save();
 
-        const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
         res.status(200).json({ sucess: true, token, user: { name: user.name } })
     } catch (error) {
         console.log(error);
@@ -39,7 +34,7 @@ export const loginUser = async (req, res) => {
 
         const isMatch = await bcrypt.compare(password, user.password)
         if (isMatch) {
-            const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
             res.status(200).json({ sucess: true, token, user: { name: user.name } })
         } else {
             return res.status(400).json({ success: false, message: "Invalid Credentials" })
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    creditBalance: { type: Number, required: true },
-})
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  creditBalance: { type: Number }, 
+});
 
-const userModel = mongoose.model.user | mongoose.model("user",userSchema)
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 
 export default userModel;
