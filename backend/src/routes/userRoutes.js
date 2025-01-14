21aec4e5f373d14e957279c5e7f3f8eb838e1852
Gokul M R <gokulmr331@gmail.com>
2025-01-14 14:25:01 +0530

    changes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { register,loginUser } from '../controller/userController.js'
+import { register,loginUser, userCredits } from '../controller/userController.js'
+import userAuth from '../middlewares/auth.js'
 const userRouter = express.Router()
 
 userRouter.post('/register',register)
 userRouter.post('/login',loginUser)
+userRouter.post('/credits',userAuth,userCredits)
+
 
 export default userRouter
