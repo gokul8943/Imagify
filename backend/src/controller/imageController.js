a393a27404fe4ec66201f1eee607368f8e0ca622
Gokul M R <gokulmr331@gmail.com>
2025-01-14 16:02:02 +0530

    api added working

---
+++ backend/src/controller/imageController.js
@@ -0,0 +1,25 @@
+import userModel from "../models/userModel"
+
+
+export const generateImage = async(req,res) =>{
+   try {
+     const {userId,prompt} = req.body
+     const user = await userModel.findById(userId)
+     if(!user || !prompt){
+        return res.json({success:false,message:"Missing Details"})
+     }
+     if(user.creditBalance ===  0  || userModel.creditBalance < 0 ){
+        return res.json({success:false ,message:"No credit Balance", creditBalance: user.creditBalance})
+     }
+     const formData = new FormData()
+     formData.append('prompt',prompt)
+
+     await axios.post("",formData,{
+        
+     })
+
+   } catch (error) {
+    console.log(error)
+    res.json({success:false,message:error.message})
+   }
+}
