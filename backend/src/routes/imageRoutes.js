b890535b862595c64bf1aa6e49913141cd3f3fa3
Gokul M R <gokulmr331@gmail.com>
2025-01-16 14:55:30 +0530

    server trashing fixes

--- backend/src/routes/imageRoutes.js
+++ backend/src/routes/imageRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { generateImage } from '../controller/imageController'
-import userAuth from '../middlewares/auth'
+import { generateImage } from '../controller/imageController.js'
+import userAuth from '../middlewares/auth.js'
 
 const imageRouter = express.Router()
 
