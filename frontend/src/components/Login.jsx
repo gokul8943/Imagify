7ee24b23e07feb0dd7651c6267637a7da588be38
Gokul M R <gokulmr331@gmail.com>
2025-02-07 00:09:40 +0530

    login component ui complete

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-       <form >
-        <h1>Login</h1>
-        <p>Welcome back! Please sign in to continue</p>
-        <div>
-            <input type='text' placeholder='Full name' required/>
+    const [state, setState] = useState("Login")
+     
+    const handleMove = () =>{
+        if(state === "Login"){
+            setState("Sign Up")
+        }else{
+            setState("Login")
+        }
+    }
+
+    return (
+        <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+            <form className='relative  bg-white p-10 rounded-xl text-slate-500'>
+                <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
+                <p>Welcome back! Please sign in to continue</p>
+                {state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
+                    <input type='text' className='outline-none text-sm ' placeholder='Full name' required />
+                </div>}
+                <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+                    <input type='email' className='outline-none text-sm ' placeholder='Email id' required />
+                </div>
+                <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
+                    <input type='password' className='outline-none text-sm ' placeholder='Password' required />
+                </div>
+                <p className='text-zm text-blue-600 my-4 cursor-pointer'>forgot password</p>
+
+                <button className='bg-blue-600 w-full text-white  py-2 rounded-full'>{state === 'Login' ? 'login' : 'Create account'}</button>
+
+                {state === "Login" ? <p className='mt-5 text-center'>Don't have an account <span className='text-blue-600 cursor-pointer' onClick={handleMove}>Sign up</span></p> :
+                    <p className='mt-5 text-center'>Already have an account <span className='text-blue-600 cursor-pointer' onClick={handleMove}>Login</span></p>}
+                <span className='absolute top-5 right-5 cursor-pointer'>❌</span>
+            </form>
         </div>
-       </form>
-    </div>
-  )
+    )
 }
 
 export default Login
