25d10fb55d8096c3334767a213e55ceba0692568
Gokul M R <gokulmr331@gmail.com>
2025-02-09 13:36:22 +0530

    changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
 
-const { user,setShowLogin } = useContext(AppContext) 
+const { user,setShowLogin,logout,credit } = useContext(AppContext) 
 
 
   const navigate = useNavigate()
@@ -27,14 +27,14 @@ const { user,setShowLogin } = useContext(AppContext)
         {user ?
           <div className='flex text-center gap-2 sm:gap-3'>
             <button className='flex item-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
-              <p className='text-xsd sm:text-sm font-medium text-gray-600'>Credit left :50</p>
+              <p className='text-xsd sm:text-sm font-medium text-gray-600'>Credit left :{credit}</p>
             </button>
-            <p className='text-gray-600 max-sm:hidden pl-4 '>Gouri</p>
+            <p className='text-gray-600 max-sm:hidden pl-4 '>{user.name}</p>
             <div className='relative group'>
               <img src={polo} alt="profile" className='w-10 drop-shadow' />
               <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                 <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                  <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+                  <li onClick={logout} className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
                 </ul>
               </div>
             </div>
