3744ff9efd678c37caf3461f468553700d918eab
Gokul M R <gokulmr331@gmail.com>
2025-02-10 14:25:45 +0530

    small changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
                 if (data.success) {
                     setToken(data.token)
                     setUser(data.user)
-                    localStorage.getItem('token', data.token)
+                    localStorage.setItem('token', data.token)
                     setShowLogin(false)
                 } else {
                     toast.error(data.message)
@@ -52,7 +52,7 @@ const Login = () => {
                 if (data.success) {
                     setToken(data.token)
                     setUser(data.user)
-                    localStorage.getItem('token', data.token)
+                    localStorage.setItem('token', data.token)
                     setShowLogin(false)
                 } else {
                     toast.error(data.message)
